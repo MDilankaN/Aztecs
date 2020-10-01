b8fef5ffd12db384467b5d2dd4f5fc1a0555cb1d
Dinushi97 <keshanidinushi48@gmail.com>
2020-10-01 19:20:59 +0530

    version 3.0 add navigation path for tabs

--- app/src/main/java/com/example/mad/teacherHome.java
+++ app/src/main/java/com/example/mad/teacherHome.java
@@ -1,14 +1,47 @@
 package com.example.mad;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class teacherHome extends AppCompatActivity {
 
+    Button navNews,navProfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_home);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        navNews = findViewById(R.id.btn_navi2);
+        navProfile = findViewById(R.id.btn_navi3);
+
+        //navigation for teacher news
+        navNews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent= new Intent(teacherHome.this, TeacherNews.class);
+                startActivity(intent);
+
+            }
+        });
+
+        //navigation for teacher profile
+        navProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(teacherHome.this, ActivityTeacherProfile.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
